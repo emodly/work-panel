2a16dc836bf4a0fbe7300e2b3dc5d23a543b020d
Deploy <deploy@work-panel.local>
2026-07-23 13:53:43 +0800

    Switch to new Supabase project

--- supabase-config.js
+++ supabase-config.js
@@ -1,2 +1,2 @@
-const SUPABASE_URL = 'https://ffyrhmtnccjfxixxdtkn.supabase.co';
-const SUPABASE_ANON_KEY = 'sb_publishable_7ISN6klzJfNSeom1sSmHbQ_7buJG4nW';
+﻿const SUPABASE_URL = 'https://daazndxwwtzbtjzqqkyz.supabase.co';
+const SUPABASE_ANON_KEY = 'sb_publishable_B5_6kiaqAnk9JME_5f7ySQ_g2xJGflQ';
