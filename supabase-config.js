0b3bb01977ed9a6b3ce41e7f5fa30f5140f6e559
Deploy <deploy@work-panel.local>
2026-07-23 14:35:23 +0800

    Switch to third Supabase project

--- supabase-config.js
+++ supabase-config.js
@@ -1,2 +1,2 @@
-﻿const SUPABASE_URL = 'https://daazndxwwtzbtjzqqkyz.supabase.co';
-const SUPABASE_ANON_KEY = 'sb_publishable_B5_6kiaqAnk9JME_5f7ySQ_g2xJGflQ';
+﻿const SUPABASE_URL = 'https://dzlypxnwgeaykgjruznm.supabase.co';
+const SUPABASE_ANON_KEY = 'sb_publishable_H37BkKqxIk02cprxi7Q87g_hUIaM6Xv';
